ee8f5f0021406accade1877ddc010ef2433e2851
Jurgis Juknevicius <jurgis.juknevicius@visma.com>
2024-02-06 13:12:58 +0200

    Rename ControlerRegistry -> ControllerRegistry.

--- __main__.py
+++ __main__.py
@@ -1,14 +1,14 @@
 from http.server import HTTPServer
-from controllers.controler_registry import ControlerRegistry
+from controllers.controller_registry import ControllerRegistry
 from controllers.weather_controller import WeatherController
 
 
-ControlerRegistry.register(ControlerRegistry, "/", WeatherController())
+ControllerRegistry.register(ControllerRegistry, "/", WeatherController())
 
 # Set the port
 port = 8000
 # Create an HTTP server instance
-httpd = HTTPServer(('localhost', port), ControlerRegistry)
+httpd = HTTPServer(('localhost', port), ControllerRegistry)
 
 print(f"Server started on localhost:{port}")
 httpd.serve_forever()
